4b8f027367230639b8d9c3a38326cb3b0dcf2631
tadeovelis <tadeovelis@gmail.com>
2021-10-07 00:37:21 -0300

    Agregar info necesaria para registrar SA

--- src/views/ApoderadoDashboard.js
+++ src/views/ApoderadoDashboard.js
@@ -1,9 +1,12 @@
 import { React, Component } from "react";
 
-import { Container, Grid, Paper, Divider, Typography, CircularProgress, Box, Button, Snackbar, Alert, AlertTitle, TextField } from '@mui/material';
-import LinearProgress, { linearProgressClasses } from '@mui/material/LinearProgress';
-import { styled } from '@mui/material/styles';
+import { Container, Grid, Paper, Divider, Typography, CircularProgress, Box, Button, Snackbar, Alert, AlertTitle, TextField, ListItemIcon, ListItemText, List, ListItem } from '@mui/material';
 import DescriptionIcon from '@mui/icons-material/Description';
+import GroupIcon from '@mui/icons-material/Group';
+import ApartmentIcon from '@mui/icons-material/Apartment';
+import EventIcon from '@mui/icons-material/Event';
+import RoomIcon from '@mui/icons-material/Room';
+import EmailIcon from '@mui/icons-material/Email';
 
 import '../assets/css/dashboard.css'
 
@@ -395,6 +398,64 @@ export default class ApoderadoDashboard extends Component {
                   </Grid>
                   <Grid item xs={12}>
                     <Typography>Recordá que necesitás tener la siguiente información:</Typography>
+                    <List dense={true}>
+                      <ListItem sx={{ px: 0 }}>
+                        <ListItemIcon sx={{ minWidth: '15%' }}>
+                          <ApartmentIcon />
+                        </ListItemIcon>
+                        <ListItemText
+                          primary="Nombre de la S.A."
+                        />
+                      </ListItem>
+                      <ListItem sx={{ px: 0 }}>
+                        <ListItemIcon sx={{ minWidth: '15%' }}>
+                          <EventIcon />
+                        </ListItemIcon>
+                        <ListItemText
+                          primary="Fecha de creación"
+                        />
+                      </ListItem>
+                      <ListItem sx={{ px: 0 }}>
+                        <ListItemIcon sx={{ minWidth: '15%' }}>
+                          <RoomIcon />
+                        </ListItemIcon>
+                        <ListItemText
+                          primary="Domicilio legal"
+                        />
+                      </ListItem>
+                      <ListItem sx={{ px: 0 }}>
+                        <ListItemIcon sx={{ minWidth: '15%' }}>
+                          <RoomIcon />
+                        </ListItemIcon>
+                        <ListItemText
+                          primary="Domicilio real"
+                        />
+                      </ListItem>
+                      <ListItem sx={{ px: 0 }}>
+                        <ListItemIcon sx={{ minWidth: '15%' }}>
+                          <EmailIcon />
+                        </ListItemIcon>
+                        <ListItemText
+                          primary="Email del apoderado"
+                        />
+                      </ListItem>
+                      <ListItem sx={{ px: 0 }}>
+                        <ListItemIcon sx={{ minWidth: '15%' }}>
+                          <GroupIcon />
+                        </ListItemIcon>
+                        <ListItemText
+                          primary="Información de los socios"
+                        />
+                      </ListItem>
+                      <ListItem sx={{ px: 0 }}>
+                        <ListItemIcon sx={{ minWidth: '15%' }}>
+                          <DescriptionIcon />
+                        </ListItemIcon>
+                        <ListItemText
+                          primary="Estatuto en formato docx, PDF u ODT"
+                        />
+                      </ListItem>
+                    </List>
                   </Grid>
                 </Grid>
               </Paper>
